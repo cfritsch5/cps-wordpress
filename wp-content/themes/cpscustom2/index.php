3df0f938fce67f157f3b98171f909ddb3bd03409
casey fritsch <caseyfritsch@gmail.com>
2017-10-05 12:57:29 -0700

    restart custom theme

---
+++ wp-content/themes/cpscustom2/index.php
@@ -0,0 +1,10 @@
+<?php
+  get_header();
+  ?>
+  <!-- end div opened in header so that each header image can be set by page -->
+</div>
+  <h1>I AM DEFINIANTLY IN THE index PHP PAGE</h1>
+  <?php
+  // get_template_part( 'content', get_post_format());
+  get_footer();
+  ?>
